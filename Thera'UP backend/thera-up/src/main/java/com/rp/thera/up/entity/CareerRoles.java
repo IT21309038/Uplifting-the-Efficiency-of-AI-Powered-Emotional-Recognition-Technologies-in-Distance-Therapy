2adad6c95714f21c85c41c2851a7906b29d3b6c7
Shadhir Ameen <99326335+IT21309038@users.noreply.github.com>
2025-03-16 22:54:21 +0530

    backend changes

---
+++ Thera'UP backend/thera-up/src/main/java/com/rp/thera/up/entity/CareerRoles.java
@@ -0,0 +1,22 @@
+package com.rp.thera.up.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class CareerRoles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String career_role;
+}
