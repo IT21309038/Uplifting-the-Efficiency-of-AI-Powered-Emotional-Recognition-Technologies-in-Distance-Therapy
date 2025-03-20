96fe4463add36de7302f4296c49f5a01521d2df7
Shadhir Ameen <99326335+IT21309038@users.noreply.github.com>
2025-03-20 09:41:46 +0530

    backend changes

--- Thera'UP backend/thera-up/src/main/java/com/rp/thera/up/serviceImpl/DoctorServiceImpl.java
+++ Thera'UP backend/thera-up/src/main/java/com/rp/thera/up/serviceImpl/DoctorServiceImpl.java
@@ -126,6 +126,8 @@ public class DoctorServiceImpl implements DoctorService {
         successLoginDoctorDTO.setRole(doctor.getRole());
         successLoginDoctorDTO.setCareer_roles(doctor.getCareerRoles());
 
+        //return success login doctor dto
+
         return successLoginDoctorDTO;
     }
 }
